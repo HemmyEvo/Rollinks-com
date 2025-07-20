81183659c24debe2fabf3e6817dd57c0eff33eec
HemmyEvo <141856365+HemmyEvo@users.noreply.github.com>
2025-07-20 12:24:05 +0100

    Update route.ts

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend'
 import { NextResponse } from 'next/server'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
-
+console.log('Resend API Key:', process.env.RESEND_API_KEY)
 export async function POST(req: Request) {
   try {
     const body = await req.json()
